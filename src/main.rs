f18e761a8c9d79d3878b5706c87a01aed9cb2f5d
vkroyer <vebjorn.kroyer@gmail.com>
2023-02-14 00:02:09 +0100

    refactor config implementation

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     let (query, file_path) = parse_config(&args);
 
     println!("Searching for {query}");
@@ -15,9 +15,16 @@ fn main() {
     println!("With text:\n{contents}");
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
