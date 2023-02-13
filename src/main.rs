7a67297b4666c957048122ecae5533a40392d6b0
vkroyer <vebjorn.kroyer@gmail.com>
2023-02-14 00:27:44 +0100

    error handling for reading file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
 
@@ -13,11 +14,20 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
-
-    println!("With text:\n{contents}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+
 
 struct Config {
     query: String,
